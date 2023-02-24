b0671f3e942e6809af21e33905dd1b85f64b97aa
stephen <stephn2027@gmail.com>
2023-02-24 21:17:50 +0900

    fixed some eslint errors and removed navbar from render

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,9 +1,11 @@
-import { Fragment } from 'react'
-import { Disclosure, Menu, Transition } from '@headlessui/react'
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { Fragment } from 'react';
+import { Disclosure, Menu, Transition } from '@headlessui/react';
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline';
+import Link from 'next/link';
+import Image from 'next/image';
 
-function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
+function classNames(...classes: any[]) {
+  return classes.filter(Boolean).join(' ');
 }
 
 export default function Navbar() {
@@ -11,31 +13,31 @@ export default function Navbar() {
     <>
       <div className="fixed mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 z-50">
         <div className="relative flex h-16 justify-between">
-          <div className="absolute inset-y-0 left-0 flex items-center">
-          </div>
+          <div className="absolute inset-y-0 left-0 flex items-center"></div>
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
             <div className="flex flex-shrink-0 items-center">
-              <img
+              <Image
                 className="block h-8 w-auto"
                 src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
                 alt="Your Company"
+                height={100}
+                width={100}
               />
             </div>
             <div className="ml-4 sm:ml-6 sm:flex sm:space-x-8">
               {/* Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" */}
-              <a
+              <Link
                 href="/leaderboard"
                 className="inline-flex items-center px-1 pt-1 text-sm font-medium text-gray-900"
               >
                 Leaderboard
-              </a>
+              </Link>
             </div>
           </div>
         </div>
       </div>
       {/* Spacer */}
-      <div className='h-16 w-full'></div>
+      <div className="h-16 w-full"></div>
     </>
-
-  )
+  );
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,14 @@
-import Navbar from '../components/navbar';
 import type { AppProps } from 'next/app';
 import AppContext from '../components/AppContext';
 import { useState } from 'react';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-
-  const [session,setSession] = useState();
+  const [session, setSession] = useState();
 
   return (
-    <AppContext.Provider value={{session,setSession}}>
+    <AppContext.Provider value={{ session, setSession }}>
       <div className=" min-h-screen">
-        <Navbar />
         <Component {...pageProps} />
       </div>
     </AppContext.Provider>
