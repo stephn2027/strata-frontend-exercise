712dbc8a70925c8b5363fa0c3ce9127db1539257
stephen <stephn2027@gmail.com>
2023-02-24 21:20:56 +0900

    fixed some eslint errors and removed navbar from render

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,11 @@
 import type { AppProps } from 'next/app';
-import AppContext from '../components/AppContext';
-import { useState } from 'react';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [session, setSession] = useState();
-
   return (
-    <AppContext.Provider value={{ session, setSession }}>
-      <div className=" min-h-screen">
-        <Component {...pageProps} />
-      </div>
-    </AppContext.Provider>
+    <div className=" min-h-screen">
+      <Component {...pageProps} />
+    </div>
   );
 }
 
